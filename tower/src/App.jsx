b908e1c2fc7e41bbdf4bfe03a3d54f2b9fe34e11
Martin <martindao2082@gmail.com>
2022-11-27 12:36:49 -0800

    added functionality to create game button

--- tower/src/App.jsx
+++ tower/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   //single id of doc containing quizlet card info inside collection with name of user.email
   const [docid, setdocid] = useState('');
   const [User, setUser] = useState();
+  const [loading, setloading] = useState(true);
   //either join or create or none (before user chooses)
   const [mode, setmode] = useState('none');
   const [linksubmissionloading, setlinksubmissionloading] = useState(false);
@@ -42,6 +43,7 @@ function App() {
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, user => {
       setUser(user);
+      setloading(false);
     })
     return unsub;
   })
@@ -61,10 +63,12 @@ function App() {
         if (document.querySelector('#quizletcards').innerHTML === '' && links[0]) {
           setlinksubmissionloading(false);
 
-          let startdiv = document.createElement('div');
-          startdiv.classList.add('quizletdiv');
-          startdiv.innerHTML = '<p class="quizletp" style="width: 50%">Term</p><p class="quizletp" style="width: 50%">Definition</p>';
-          document.querySelector('#quizletcardstitle').append(startdiv);
+          if (document.querySelector('#quizletcardstitle').innerHTML === '') {
+            let startdiv = document.createElement('div');
+            startdiv.classList.add('quizletdiv');
+            startdiv.innerHTML = '<p class="quizletp" style="width: 50%">Term</p><p class="quizletp" style="width: 50%">Definition</p>';
+            document.querySelector('#quizletcardstitle').append(startdiv);
+          }
 
           let array = links[0].info;
           for (let i = 0; i < array.length/2; i++) {
@@ -85,23 +89,26 @@ function App() {
   return (
     <div className="App">
         <img className='absolute w-full h-[100vh] z-[-1] object-cover blur-[1px]' src={`../images/pattern.png`}/>
-        {/* {User && <div onClick={signout} className='hover hover:underline rounded-[15px] bg-gray-400 text-xl absolute right-5 top-3 text-white font-bold px-4 py-2'>Log out</div>} */}
-      {!User && <h2 className='absolute top-[20%] left-2/4 z-[3] -translate-x-2/4 -translate-y-2/4 text-[3rem] w-full md:text-[4rem] font-bold text-white text-center'>Welcome to my Tower Game!</h2>}
-      {
+        {User && <div onClick={signout} className='hover hover:underline rounded-[15px] bg-gray-400 text-xl absolute right-5 top-3 text-white font-bold px-4 py-2'>Log out</div>}
+        {!loading && !User && <h2 className='absolute top-[20%] left-2/4 z-[3] -translate-x-2/4 -translate-y-2/4 text-[3rem] w-full md:text-[4rem] font-bold text-white text-center'>Welcome to my Tower Game!</h2>}
+      { loading ? <div></div> :
         User ? <div id="infobar" className='absolute flex flex-col items-center bg-blue-600 w-[80%] h-[80%] top-2/4 left-2/4 z-[3] -translate-x-2/4 -translate-y-2/4'>
-          <p className='text-white text-[4rem] mt-8'>Menu</p>
-          <div className='bg-gradient-to-r from-green-400 to-green-600 rounded-[15px] text-white font-bold text-3xl px-8 py-2 mb-8 mt-10 hover hover:underline'>Create Game</div>
-          <div className='bg-gradient-to-r from-green-400 to-green-600 rounded-[15px] text-white font-bold text-3xl px-8 py-2 hover hover:underline'>Join Game</div>
+          <div className='flex flex-col justify-center items-center' id="menu">
+            <p className='text-white text-[4rem] mt-8'>Menu</p>
+            <div onClick={() => {setmode('create'); document.querySelector('#menu').style.display = 'none'}} className='bg-gradient-to-r from-green-400 to-green-600 rounded-[15px] text-white font-bold text-3xl px-8 py-2 mb-8 mt-10 hover hover:underline'>Create Game</div>
+            <div className='bg-gradient-to-r from-green-400 to-green-600 rounded-[15px] text-white font-bold text-3xl px-8 py-2 hover hover:underline'>Join Game</div>
+          </div>
           
-          <div id="creategame" className='w-full' style={{display: mode === 'create' ? block : 'none'}}>
+          <div id="creategame" className='w-full' style={{display: mode === 'create' ? 'block' : 'none'}}>
             {
               linksubmissionloading ? <div className='flex flex-col justify-center items-center'>
                 <p className='text-2xl mb-2'>Loading your flashcard info</p>
                 <img className='w-[25%]' src="https://media0.giphy.com/media/xTk9ZvMnbIiIew7IpW/giphy.gif"/>
               </div> : <div className='flex flex-col items-center w-full mt-4'>
-                <p className='text-2xl'>Enter link of your quizlet</p>
-                <input className='w-[60%] my-2 border-solid border-black border-2 px-2' ref={text} type="text"/>
-                <input className='hover border-solid mb-8 text-xl bg-blue-400 font-bold text-white px-[4rem] py-[0.2rem] rounded-[20px] mt-2' onClick={submitClick} type="submit"/>
+                <div onClick={() => {setmode('none'); document.querySelector('#menu').style.display = 'flex'}} className='text-black font-bold absolute top-2 left-4 text-xl hover hover:underline'>Back to Menu</div>
+                <p className='text-2xl text-white'>Enter link of your quizlet</p>
+                <input className='w-[60%] my-2 border-solid border-black border-2 px-2 bg-white' ref={text} type="text"/>
+                <input className='hover hover:underline border-solid mb-8 text-xl bg-green-500 font-bold text-white px-[4rem] py-[0.2rem] rounded-[20px] mt-2' onClick={submitClick} type="submit"/>
                 <div id="quizletcardscontainer">
                   <div id="quizletcardstitle"></div>
                   <div id="quizletcards"></div>
